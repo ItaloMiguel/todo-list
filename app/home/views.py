18394abca234266bfdcd5d5bb97bde7f0b64a19c
ItaloMiguel <italomiguelleite16@gmail.com>
2023-03-09 09:01:00 +0000

    Descobrindo agora que posso colocar os templates na pastas

--- app/home/views.py
+++ app/home/views.py
@@ -4,10 +4,10 @@ from django.contrib import messages
 def home(request):
     if request.method == 'GET':
 
-        return render(request, 'home/index.html')
+        return render(request, 'index.html')
     if request.method == 'POST':
 
-        return render(request, 'home/index.html')
+        return render(request, 'index.html')
     
-    return render(request, 'home/index.html')
+    return render(request, 'index.html')
     
